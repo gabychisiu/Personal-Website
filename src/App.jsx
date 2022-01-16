7dec55cae2dd82360f362cba0f34bb775a616ab0
gabychisiu <chisiugaby@gmail.com>
2022-01-16 23:54:20 +0200

    Theme and small bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -6,42 +6,63 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 import { AboutPage } from "./components/Pages/AboutPage";
 import { HomePage } from "./components/Pages/HomePage";
 import { SkillsPage } from "./components/Pages/SkillsPage";
 import { WorkPage } from "./components/Pages/WorkPage";
 import { ContactPage } from "./components/Pages/ContactPage";
 
+export const lightTheme = createTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: "#004d40",
+      light: "#346d63",
+      dark: "#002f24",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#00698f",
+      light:"#3489a6",
+      dark: "#013f56",
+      contrastText: "#fff",
+    },
+  },
+});
+
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Switch>
-          <Route path="/about">
-            <AboutPage />
-          </Route>
-          <Route path="/home">
-            <HomePage />
-          </Route>
-          <Route path="/skills">
-            <SkillsPage />
-          </Route>
-          <Route path="/work">
-            <WorkPage />
-          </Route>
-          <Route path="/contact">
-            <ContactPage
-              name="Chisiu Marius Gabriel"
-              address="Cluj-Napoca"
-              email="chisiugaby@gmail.com"
-              phone="+40758254399"
-            />
-          </Route>
-          <Redirect to="/home" />
-        </Switch>
-      </div>
-    </Router>
+    <ThemeProvider theme={lightTheme}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route path="/about">
+              <AboutPage />
+            </Route>
+            <Route path="/home">
+              <HomePage />
+            </Route>
+            <Route path="/skills">
+              <SkillsPage />
+            </Route>
+            <Route path="/work">
+              <WorkPage />
+            </Route>
+            <Route path="/contact">
+              <ContactPage
+                name="Chisiu Marius Gabriel"
+                address="Cluj-Napoca"
+                email="chisiugaby@gmail.com"
+                phone="+40758254399"
+              />
+            </Route>
+            <Redirect to="/home" />
+          </Switch>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -3,7 +3,7 @@ import atvs from "./atvs.png";
 import users from "./users.png";
 import mario from "./mario.png";
 import * as React from "react";
-import ImageList from "@mui/material/ImageList";
+import { ImageList, Box } from "@mui/material";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
 import IconButton from "@mui/material/IconButton";
@@ -11,37 +11,43 @@ import { FaGithub } from "react-icons/fa";
 
 export const Work = () => {
   return (
-    <ImageList className="image-list">
-      <ImageListItem key="Subheader" cols={3}></ImageListItem>
-      {itemData.map((item) => (
-        <ImageListItem key={item.img}>
-          <img
-            src={`${item.img}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title={item.title}
-            subtitle={item.author}
-            actionIcon={
-              <IconButton
-                sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                aria-label={`info about ${item.title}`}
-                href={item.link}
-                target="_blank"
-              >
-                <FaGithub />
-              </IconButton>
-            }
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
+    <Box>
+      <ImageList cols={3} className="image-list">
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}>
+            <img
+              src={`${item.img}?w=200&fit=crop&auto=format`}
+              srcSet={`${item.img}?w=200&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.title}
+              loading="lazy"
+            />
+            <ImageListItemBar
+              title={item.title}
+              subtitle={item.author}
+              actionIcon={
+                <IconButton
+                  sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                  aria-label={`info about ${item.title}`}
+                  href={item.link}
+                  target="_blank"
+                >
+                  <FaGithub />
+                </IconButton>
+              }
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </Box>
   );
 };
 
 const itemData = [
+  {
+    img: atvs,
+    title: "Personal projects",
+    link: "https://github.com/gabychisiu/Personal-Website",
+  },
   {
     img: atvs,
     title: "Atv",
